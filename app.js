25a3f3997f00f96473a3c3e387f53016d65d04d7
Ravdeep Singh <ravdeep@iotasol.com>
2023-06-29 11:37:37 +0530

    init commit

--- app.js
+++ app.js
@@ -1,61 +1,106 @@
+const { default: axios } = require("axios");
+const bodyParser = require("body-parser");
 const express = require("express");
+
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.type('html').send(html));
+const clientId = "77ysyjuj4aeszo";
+const clientSecret = "V4QzAh5iL04EPx3U";
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => res.type("html").send("hi"));
+app.get("/auth/linkedin/callback", async (req, res) => {
+  const { code } = req.query;
+
+  let fname = "";
+  let lname = "";
+  let profilePictureUrl = null;
+  let email = "";
+
+  try {
+    const accessTokenRequest = await axios.post(
+      "https://www.linkedin.com/oauth/v2/accessToken",
+      {
+        grant_type: "authorization_code",
+        code,
+        redirect_uri: "http://192.168.29.31:3000/auth/linkedin/callback",
+        client_id: clientId,
+        client_secret: clientSecret,
+      },
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+    const accessToken = accessTokenRequest.data.access_token;
+    const { data } = await axios.get("https://api.linkedin.com/v2/me", {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      params: {
+        projection:
+          "(localizedFirstName,localizedLastName,id,profilePicture(displayImage~:playableStreams),organizations)",
+      },
+    });
+
+    const { data: emailAddressData } = await axios.get(
+      "https://api.linkedin.com/v2/clientAwareMemberHandles",
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        params: {
+          q: "members",
+          projection: "(elements*(primary,type,handle~))",
+        },
+      }
+    );
+
+    email = emailAddressData?.elements[0]["handle~"].emailAddress;
+
+    if (data.profilePicture) {
+      const profilePictures = data.profilePicture["displayImage~"].elements;
+      profilePictureUrl =
+        !!profilePictures.length &&
+        profilePictures[profilePictures.length - 1].identifiers[0].identifier;
+    }
+
+    fname = data.localizedFirstName;
+    lname = data.localizedLastName;
+    console.log({ email, fname, lname, profilePictureUrl });
+  } catch (err) {
+    console.log(err);
+    return res.redirect(
+      `exp://192.168.29.31:19000?fname=${fname}&lname=${lname}&photoUrl=${encodeURIComponent(
+        profilePictureUrl
+      )}&email=${email}`
+    );
+  }
+
+  return res.redirect(
+    `exp://192.168.29.31:19000?fname=${fname}&lname=${lname}&photoUrl=${encodeURIComponent(
+      profilePictureUrl
+    )}&email=${email}`
+  );
+});
+
+app.get("/auth/apple/callback", async (req, res) => {
+  console.log("chal ja get", JSON.stringify(req.query));
+  return res.redirect(`exp://192.168.1.14:19000?fname=test`);
+});
+
+app.post("/auth/apple/callback", async (req, res) => {
+  console.log("chal ja post", JSON.stringify(req.body));
+  return res.redirect(`exp://192.168.1.14:19000?fname=test`);
+});
+
+const server = app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`)
+
+);
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
-
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
